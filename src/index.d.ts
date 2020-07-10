9d4ba61436a6367e50e0ea9a15f139c8f2defabb
Eric-LLi <ericli1116@gmail.com>
2020-07-10 16:42:30 +1000

    update print funticon

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,5 @@ export declare function sampleMethod(
 	numberArgument: number,
 	callback: (str: string, num: number) => void
 ): void;
+
+export declare function print(printerID: string, macAddress: string, text: string): Promise<boolean>;
--- src/index.js
+++ src/index.js
@@ -6,4 +6,8 @@ export const sampleMethod = (str, num, callback) => {
 	IntermecPrinter.sampleMethod(str, num, callback);
 };
 
+export const print = (printerID, macAddress, text) => {
+	return IntermecPrinter.print(printerID, macAddress, text);
+};
+
 export default IntermecPrinter;
