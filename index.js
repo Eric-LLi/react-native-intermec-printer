908b89e4bbc62f3b154c77c44bd821d2ac78e647
Eric-LLi <ericli1116@gmail.com>
2020-07-10 15:13:52 +1000

    update

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ import { NativeModules } from 'react-native';
 
 const { IntermecPrinter } = NativeModules;
 
-export default IntermecPrinter;
+export const sampleMethod = (str, num) => {
+	IntermecPrinter.sampleMethod(str, num);
+};
+
+// export default IntermecPrinter;
