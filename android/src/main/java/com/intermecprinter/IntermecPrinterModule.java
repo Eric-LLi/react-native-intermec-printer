de723c9d1b5162b41f1dda3430676ad4ad3c17d0
Eric-LLi <ericli1116@gmail.com>
2020-07-11 01:35:08 +1000

    update print input

--- android/src/main/java/com/intermecprinter/IntermecPrinterModule.java
+++ android/src/main/java/com/intermecprinter/IntermecPrinterModule.java
@@ -92,7 +92,8 @@ public class IntermecPrinterModule extends ReactContextBaseJavaModule {
 	}
 
 	@ReactMethod
-	public void print(String printerID, String macAddress, String title, String barcode, String ticket_type,
+	public void print(String profile, String printerID, String macAddress, String title,
+	                  String barcode, String ticket_type,
 	                  final Promise promise) {
 		rPromise = promise;
 		// Create a PrintTask to do printing on a separate thread.
@@ -100,7 +101,7 @@ public class IntermecPrinterModule extends ReactContextBaseJavaModule {
 
 		// Executes PrintTask with the specified parameter which is passed
 		// to the PrintTask.doInBackground method.
-		task.execute(printerID, macAddress, title, barcode, ticket_type);
+		task.execute(profile, printerID, macAddress, title, barcode, ticket_type);
 	}
 
 
@@ -132,20 +133,21 @@ public class IntermecPrinterModule extends ReactContextBaseJavaModule {
 		 */
 		@Override
 		protected String doInBackground(String... args) {
-			String profiles = jsonCmdAttribStr.trim();
+//			String profiles = jsonCmdAttribStr.trim();
+			String profiles = args[0];
+			String sPrinterID = args[1];
+			String sPrinterURI = "bt://" + formatMacAddress(args[2]);
+
+			String sTitle = args[3];
+			String sBarcode = args[4];
+			String sTicketType = args[5];
 
 			LabelPrinter lp = null;
 			String sResult = null;
-			String sPrinterID = args[0];
-			String sPrinterURI = "bt://" + formatMacAddress(args[1]);
-
-			String sTitle = args[2];
-			String sBarcode = args[3];
-			String sTicketType = args[4];
 
 			if (BuildConfig.DEBUG)
 				Log.d(TAG, "Printing to printer id " + sPrinterID + " with uri " + sPrinterURI +
-						" and itemName " + sItemName + " and itemNo " + sItemNo);
+						" and itemName " + sTitle + " and itemNo " + sBarcode);
 
 			LabelPrinter.ExtraSettings exSettings = new LabelPrinter.ExtraSettings();
 			exSettings.setContext(reactContext);
