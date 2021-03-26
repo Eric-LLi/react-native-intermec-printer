be66e988aad069b66ad4a78bcf5da9cce985a970
Eric-LLi <ericli1116@gmail.com>
2021-03-26 11:56:19 +1100

    <UPDATE>

--- src/index.js
+++ src/index.js
@@ -11,19 +11,33 @@ const events = {};
 
 const eventEmitter = new NativeEventEmitter(IntermecPrinter);
 
+// IntermecPrinter.on = (event, handler) => {
+// 	const eventListener = eventEmitter.addListener(event, handler);
+
+// 	events[event] = eventListener;
+// };
+
+// IntermecPrinter.off = (event, handler) => {
+// 	if (events.hasOwnProperty(event)) {
+// 		const eventListener = events[event];
+
+// 		eventListener.remove();
+
+// 		delete events[event];
+// 	}
+// };
+
 IntermecPrinter.on = (event, handler) => {
 	const eventListener = eventEmitter.addListener(event, handler);
 
-	events[event] = eventListener;
+	events[event] =  events[event] ? [...events[event], eventListener]: [eventListener];
 };
 
-IntermecPrinter.off = (event, handler) => {
-	if (events.hasOwnProperty(event)) {
-		const eventListener = events[event];
+IntermecPrinter.off = (event) => {
+	if (Object.hasOwnProperty.call(events, event)) {
+		const eventListener = events[event].shift();
 
-		eventListener.remove();
-
-		delete events[event];
+		if(eventListener) eventListener.remove();
 	}
 };
 
