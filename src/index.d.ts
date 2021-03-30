000b01d63db776d4dde6afa7a99fc2dab9fe2c20
Eric-LLi <ericli1116@gmail.com>
2021-03-30 15:03:27 +1100

    <UPDATE>

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,9 +1,3 @@
-export declare function sampleMethod(
-	stringArgument: string,
-	numberArgument: number,
-	callback: (str: string, num: number) => void
-): void;
-
 export declare function init(): () => void;
 
 export declare function disconnect(): () => void;
